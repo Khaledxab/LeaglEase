fd0937886ac2845b4571deeb6b1e172707920791
khaledxab <khaledxab@gmail.com>
2023-04-07 19:20:06 +0200

    signup page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:legalease/screens/login_screen.dart';
+import 'package:legalease/screens/registration_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      home: RegistrationScreen(),
     );
   }
 }
