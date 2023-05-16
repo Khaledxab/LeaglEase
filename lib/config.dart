ce0b9cb7f350c928c90d5df25162c6bb3550246a
khaledxab <khaledxab@gmail.com>
2023-05-16 02:19:07 +0100

    configs

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,7 @@
 final url = 'http://192.168.1.101:3000/';
 final registration = url + "registration";
+final registrationAD = url + "registrationAD";
 final login = url + 'login';
 final users = url + 'users';
 final data = url + 'data';
+final lyawyers = url + 'Lawyers';
